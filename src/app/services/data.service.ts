6ef5aa0e1aa45f1f5904cdf308e3fda7f7b13708
lazzzaroni <iamnotlazzaroni@gmail.com>
2022-05-09 16:26:10 +0200

    created dataServise

--- src/app/services/data.service.ts
+++ src/app/services/data.service.ts
@@ -1,9 +1,9 @@
 import { Injectable } from '@angular/core';
+import data from '../../assets/data.json';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class DataService {
-
-  constructor() { }
+  dataBase: any = data;
 }
