3e15cc33e3243f8e208c3cb8d034d6ee8e555b75
lazzzaroni <iamnotlazzaroni@gmail.com>
2022-05-09 09:40:58 +0200

    init commit + adding tailwindcss & deps

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  content: [],
+  theme: {
+    fontFamily: {
+      sans: ["Manrope", "system-ui", "sans-serif"],
+    },
+    extend: {},
+  },
+  plugins: [require("@tailwindcss/forms")],
+};
