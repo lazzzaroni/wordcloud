aff7b1e8c0c18d0554ede83ff2aa358b3c823269
lazzzaroni <iamnotlazzaroni@gmail.com>
2022-05-09 12:13:29 +0200

    added routing between pages

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { GameComponent } from './components/game/game.component';
+import { ResultsComponent } from './components/results/results.component';
+import { StartComponent } from './components/start/start.component';
+
+const routes: Routes = [
+  { path: 'start', component: StartComponent },
+  { path: 'game', component: GameComponent },
+  { path: 'results', component: ResultsComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
