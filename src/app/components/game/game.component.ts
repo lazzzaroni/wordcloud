6c70c1992d205a2acbe63018f7d5bb82cf5f4ed6
lazzzaroni <iamnotlazzaroni@gmail.com>
2022-05-10 10:35:37 +0200

    working on GameComponent

--- src/app/components/game/game.component.ts
+++ src/app/components/game/game.component.ts
@@ -1,12 +1,26 @@
 import { Component, OnInit } from '@angular/core';
 
+import { DataService } from 'src/app/services/data.service';
+import { IGameData } from 'src/app/interfaces/game.interface';
+
 @Component({
   selector: 'wordcloud-game',
   templateUrl: './game.component.html',
   styleUrls: ['./game.component.css'],
 })
 export class GameComponent implements OnInit {
-  constructor() {}
+  data: any;
+  getRandomNum: number;
+  gameData: IGameData;
+  allWords: string[];
+  goodWords: string[] = [];
+
+  constructor(private dataService: DataService) {
+    this.data = this.dataService.dataBase; // set data.json from DataServise to the const
+    this.getRandomNum = Math.floor(Math.random() * this.data.length); // set number of random object with info about question from data.json
+    this.gameData = this.data[this.getRandomNum]; // extract data for current session
+    this.allWords = this.gameData.all_words;
+  }
 
   ngOnInit(): void {}
 }
