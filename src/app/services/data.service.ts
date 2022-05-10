9f38ff8e70080584edb63cfc53e79a830cb9dacc
lazzzaroni <iamnotlazzaroni@gmail.com>
2022-05-10 10:39:25 +0200

    added type to database and userName property

--- src/app/services/data.service.ts
+++ src/app/services/data.service.ts
@@ -1,9 +1,14 @@
 import { Injectable } from '@angular/core';
+
 import data from '../../assets/data.json';
+import { IGameData } from '../interfaces/game.interface';
 
 @Injectable({
   providedIn: 'root',
 })
 export class DataService {
-  dataBase: any = data;
+  dataBase: IGameData = data;
+  userName: string | undefined;
+
+  constructor() {}
 }
