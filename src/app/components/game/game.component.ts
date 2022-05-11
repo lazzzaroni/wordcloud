8e2829facf653c33b17e1f2674a4efca2097d2b0
lazzzaroni <iamnotlazzaroni@gmail.com>
2022-05-11 12:37:19 +0200

    working on game component

--- src/app/components/game/game.component.ts
+++ src/app/components/game/game.component.ts
@@ -9,17 +9,29 @@ import { IGameData } from 'src/app/interfaces/game.interface';
   styleUrls: ['./game.component.css'],
 })
 export class GameComponent implements OnInit {
-  data: any;
-  getRandomNum: number;
-  gameData: IGameData;
-  allWords: string[];
-  goodWords: string[] = [];
+  data!: IGameData[];
+  getRandomNum!: number;
+  gameData!: IGameData;
+  allWords!: string[];
+  goodWords!: string[];
 
   constructor(private dataService: DataService) {
-    this.data = this.dataService.dataBase; // set data.json from DataServise to the const
+    if (!this.dataService.dataBase) {
+      this.dataService.getData().subscribe((res) => {
+        this.data = res;
+        this.setWords();
+      });
+    } else {
+      this.data = this.dataService.dataBase;
+      this.setWords();
+    }
+  }
+
+  setWords() {
     this.getRandomNum = Math.floor(Math.random() * this.data.length); // set number of random object with info about question from data.json
     this.gameData = this.data[this.getRandomNum]; // extract data for current session
     this.allWords = this.gameData.all_words;
+    this.goodWords = this.gameData.good_words;
   }
 
   ngOnInit(): void {}
