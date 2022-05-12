2ea93f885920994905bbf52d78fa449e0db23ae4
lazzzaroni <iamnotlazzaroni@gmail.com>
2022-05-12 09:59:37 +0200

    added service for operations with answers

--- src/app/services/words.service.ts
+++ src/app/services/words.service.ts
@@ -1,9 +1,28 @@
 import { Injectable } from '@angular/core';
 
+import { IGameData } from '../interfaces/game.interface';
+
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class WordsService {
+  constructor() {}
 
-  constructor() { }
+  getGameData(data: IGameData) {
+    return {
+      question: data.question,
+      words: this.getAnswers(data),
+    };
+  }
+
+  getAnswers(data: IGameData) {
+    return data.all_words.map((el) => {
+      return {
+        word: el,
+        checked: false,
+        correct: data.good_words.includes(el),
+        result: 0,
+      };
+    });
+  }
 }
