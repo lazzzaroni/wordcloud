adc7043806799910b7a15df20c7bfa6469f8c904
lazzzaroni <iamnotlazzaroni@gmail.com>
2022-05-12 09:58:02 +0200

    set userName & score on init

--- src/app/services/player.service.ts
+++ src/app/services/player.service.ts
@@ -4,7 +4,8 @@ import { Injectable } from '@angular/core';
   providedIn: 'root',
 })
 export class PlayerService {
-  name: string = '';
+  name: string = 'Mystery Guest';
+  score: number = 666;
 
   constructor() {}
 }
